ef798bfaad4d9ba0ef021aa7a43cd23808c3bd4d
BlackDev0222 <BlackDev0222@gmail.com>
2021-07-08 15:40:21 -0600

    update test\setup.js

---
+++ test/setup.js
@@ -0,0 +1,19 @@
+let mongoose = require('mongoose')
+let { dropDatabase, batchInsert } = require('../tasks/seed_db_script')
+
+before('connect to and seed test database', (done) => {
+  mongoose.connect(config.dbPath, (err) => {
+    dropDatabase()
+
+    batchInsert({ size: {
+      projects: 5,
+      boards: 5,
+      tasks: 6
+    }, log: true }, (err, items) => {
+      if (err) return done(err)
+      global.seeds = items
+      console.log("Database seeding done.\n")
+      done()
+    })
+  })
+})
