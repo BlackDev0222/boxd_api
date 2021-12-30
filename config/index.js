5adb7c3fe2c0880b4f97e85f0955612c9c7db196
BlackDev0222 <BlackDev0222@gmail.com>
2021-12-30 14:40:24 -0700

    update config\index.js

---
+++ config/index.js
@@ -0,0 +1,35 @@
+var config = {
+  base: {
+    port: process.env.$PORT || process.env.PORT || 3000,
+  },
+
+  development: {
+    appURL: 'http://localhost:3000',
+    devEmail: 'dank@memer.com',
+  },
+
+  test: {
+    appURL: 'http://localhost:3000',
+    devEmail: 'dank@memer.com',
+  },
+
+  production: {
+    appURL: 'https://boxd-api.ivorreic.com',
+  },
+};
+
+try {
+  var secrets = require('./secrets');
+} catch (err) {
+  var secrets = _.pick(process.env, [
+    'googleOAuthClientID',
+    'googleOAuthSecret',
+    'JWTSecret',
+    'adminEmails',
+    'dbPath',
+  ]);
+}
+
+var cf = _.merge(config.base, config[process.env.NODE_ENV], secrets);
+
+module.exports = cf;
