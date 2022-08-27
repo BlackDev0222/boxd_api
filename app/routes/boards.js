dbced26a28ca36a634970f16c67449d04ce33c46
BlackDev0222 <BlackDev0222@gmail.com>
2022-08-27 15:40:32 -0600

    update app\routes\boards.js

---
+++ app/routes/boards.js
@@ -0,0 +1,32 @@
+let router = require('express').Router()
+let { board, shared: $ } = require('../models')
+
+router.post('/',
+  $.getItem('project'),
+  $.validateAccess(),
+  $.addFirstAdmin,
+  $.createItem('board'),
+  $.respond('omit:project')
+)
+
+router.get('/:id',
+  $.getItem('board'),
+  $.getItem('project', '$.board.project_id'),
+  $.validateAccess(),
+  board.setQueryArgs('lists'), $.getItems('list'),
+  board.setQueryArgs('tasks'), $.getItems('task'),
+  board.setQueryArgs('users'), $.getItems('user'),
+  $.respond()
+)
+
+router.put('/:id',
+  $.getItem('board'),
+  $.validateAccess('admin', 'allowPassage'),
+  $.updateUserAdmin('board'),
+  board.updateLabel,
+  $.updateItem('board', 'omit:users:project_id'),
+  $.saveItem('board'),
+  $.respond()
+)
+
+module.exports = router
